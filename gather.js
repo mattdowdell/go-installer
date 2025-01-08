bc515f7abb501b98873bc5d96d90d072e5b19ff5
Matt Dowdell <mdowdell244@gmail.com>
2025-01-08 09:29:40 +0000

    gather fixes

--- gather.js
+++ gather.js
@@ -1,3 +1,5 @@
+/*global module, process*/
+
 /**
  * Gathers 3 pieces of information:
  *
@@ -5,89 +7,81 @@
  * - The version, converting 'latest' to an actual version to support cache invalidation.
  * - The cache key based on the name, version and runner.
  */
-module.exports = async ({core, exec}) => {
-	const pkg = process.env.package;
+module.exports = async ({ core, exec }) => {
+  const pkg = process.env.package;
 
-	const name = getBinName(pkg)
-	core.setOutput('name', name);
+  const name = getBinName(pkg);
+  core.setOutput("name", name);
 
-	const version = process.env.version;
-	if (version != 'latest') {
-		core.setOutput('version', version);
-		core.setOutput('key', makeKey({core, name, version}));
+  const version = process.env.version;
+  if (version != "latest") {
+    core.setOutput("version", version);
+    core.setOutput("key", makeKey({ core, name, version }));
 
-		return
-	}
+    return;
+  }
 
-	const paths = getModules(pkg);
+  for (const mod of getModules()) {
+    const result = await exec.getExecOutput(
+      "go",
+      ["list", "-m", "-versions", "-mod=readonly", "-json", mod],
+      { ignoreReturnCode: true },
+    );
 
-	for (const mod of getModules()) {
-		const result = await exec.getExecOutput(
-			'go',
-			['list', '-m', '-versions', '-mod=readonly', '-json',  mod],
-			{ ignoreReturnCode: true }
-		);
+    if (result.code == 0) {
+      const data = JSON.parse(result.stdout);
+      // google.golang.org returns success when not in the module root
+      if (!Object.hasOwn(data, "Versions")) {
+        continue;
+      }
 
-		if (result.code == 0) {
-			const data = JSON.parse(result.stdout);
-			// google.golang.org returns success when not in the module root
-			if (!data.hasOwnProperty('Versions')) {
-				continue;
-			}
+      const version = data.Versions[data.Versions.length - 1];
 
-			const version = data.Versions[data.Versions.length - 1];
+      core.setOutput("version", version);
+      core.setOutput("key", makeKey({ core, name, version }));
 
-			core.setOutput('version', version);
-			core.setOutput('key', makeKey({core, name, version}));
+      return;
+    }
+  }
 
-			return;
-		}
-	}
-
-	core.setFailed('failed to identify go module');
-}
+  core.setFailed("failed to identify go module");
+};
 
 function makeKey({ core, name, version }) {
-	return [
-		"go-install"
-		core.platform,
-		core.arch,
-		name,
-		version,
-	].join('-');
+  return ["go-install", core.platform, core.arch, name, version].join("-");
 }
 
 /**
  * Get the name of the binary to be installed.
  */
 function getBinName(pkg) {
-	const parts = pkg.split('/');
+  const parts = pkg.split("/");
 
-	let name = parts[parts.length - 1];
-	if (/v\d+/.test(name)) {
-		name = parts[parts.length - 1];
-	}
+  let name = parts[parts.length - 1];
+  if (/v\d+/.test(name)) {
+    name = parts[parts.length - 1];
+  }
 
-	return name
+  return name;
 }
 
 /**
  * Get the potential module paths to get a version from.
  */
 function getModules(pkg) {
-	const mods = [];
+  const mods = [];
 
-	while (pkg.lastIndexOf('/') > -1) {
-		mods.push(pkg)
-		pkg = parent(pkg)
-	}
+  while (pkg.lastIndexOf("/") > -1) {
+    mods.push(pkg);
+    pkg = parent(pkg);
+  }
 
-	return mods
+  return mods;
 }
 
 /**
  * Get the parent of the given package path.
  */
 function parent(pkg) {
-	return pkg.split('/').slice(0, -1).join('/')
+  return pkg.split("/").slice(0, -1).join("/");
 }
