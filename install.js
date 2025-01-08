804999b70440d698c4f79ea54e3f4c87247fd4a1
Matt Dowdell <mdowdell244@gmail.com>
2025-01-08 09:30:26 +0000

    lint,fmt install

--- install.js
+++ install.js
@@ -1,9 +1,11 @@
+/*global module, process*/
+
 /**
  * Installs a Go binary at the selected version.
  */
-module.exports = async ({exec}) => {
-	const pkg = process.env.package;
-	const version = process.env.version;
+module.exports = async ({ exec }) => {
+  const pkg = process.env.package;
+  const version = process.env.version;
 
-	await exec.exec('go', ['install', `${pkg}@${version}`])
-}
+  await exec.exec("go", ["install", `${pkg}@${version}`]);
+};
