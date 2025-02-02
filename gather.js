970a5c8551a04c622b92d99e00f34fb886d7edfd
Matthew Dowdell <mattdowdell@users.noreply.github.com>
2025-02-02 08:57:08 +0000

    Implement changes suggested by zizmor (#5)
    Implement changes suggested by `zizmor --pedantic`:
    
    - Use pinned actions.
    - Avoid issues from template expansion.
    
    Other changes
    - Remove `os` dependency in favour of environment variables.
    
    See #6 for a follow up to integrate this into CI.

--- gather.js
+++ gather.js
@@ -7,7 +7,7 @@
  * - The version, converting 'latest' to an actual version to support cache invalidation.
  * - The cache key based on the name, version and runner.
  */
-module.exports = async ({ core, exec, fs, os, path }) => {
+module.exports = async ({ core, exec, fs, path }) => {
   const pkg = process.env.package;
 
   const name = getBinName(pkg);
@@ -16,7 +16,7 @@ module.exports = async ({ core, exec, fs, os, path }) => {
   const version = process.env.version;
   if (version != "latest") {
     core.setOutput("version", version);
-    core.setOutput("key", makeKey({ os, name, version }));
+    core.setOutput("key", makeKey({ name, version }));
 
     return;
   }
@@ -26,7 +26,7 @@ module.exports = async ({ core, exec, fs, os, path }) => {
 
   try {
     versions = await parseVersionFile({ core, exec, fs, path, versionFile });
-  } catch(e) {
+  } catch (e) {
     core.setFailed(e);
     return;
   }
@@ -47,7 +47,9 @@ module.exports = async ({ core, exec, fs, os, path }) => {
       }
 
       if (versionFile != "" && !versions.has(mod)) {
-        core.setFailed(`version-file ${versionFile} is missing package: ${mod}`);
+        core.setFailed(
+          `version-file ${versionFile} is missing package: ${mod}`,
+        );
         return;
       }
 
@@ -57,7 +59,7 @@ module.exports = async ({ core, exec, fs, os, path }) => {
       }
 
       core.setOutput("version", version);
-      core.setOutput("key", makeKey({ os, name, version }));
+      core.setOutput("key", makeKey({ name, version }));
 
       return;
     }
@@ -76,7 +78,6 @@ async function parseVersionFile({ core, exec, fs, path, versionFile }) {
 
   if (path.basename(versionFile) != "go.mod") {
     throw new Error(`version-file is not a go.mod file: ${versionFile}`);
-
   }
 
   if (!fs.existsSync(versionFile)) {
@@ -88,19 +89,27 @@ async function parseVersionFile({ core, exec, fs, path, versionFile }) {
     ["list", "-mod=readonly", "-m", "-json", "all"],
     { cwd: path.dirname(versionFile) },
   );
-  const data = JSON.parse("[" + result.stdout.replaceAll("}\n{", "},\n{") + "]");
+  const data = JSON.parse(
+    "[" + result.stdout.replaceAll("}\n{", "},\n{") + "]",
+  );
 
-  return new Map(data.map(d => [d.Path, d.Version]));
+  return new Map(data.map((d) => [d.Path, d.Version]));
 }
 
 /**
  * Build the key to use for cache lookups.
  *
- * Ideally, this would use core instead of os, but that requires @actions/core v1.11.0.
+ * Ideally, this would use core instead of process.env, but that requires @actions/core v1.11.0.
  * actions/github-script@v7.0.1 provides @actions/core v1.10.1, which does provide this info.
  */
-function makeKey({ os, name, version }) {
-  return ["go-installer", os.platform(), os.arch(), name, version].join("-");
+function makeKey({ name, version }) {
+  return [
+    "go-installer",
+    process.env.RUNNER_OS,
+    process.env.RUNNER_ARCH,
+    name,
+    version,
+  ].join("-");
 }
 
 /**
